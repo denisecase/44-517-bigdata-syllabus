9945659d79d1a826789f26a4b1d5fd8bacbbdcbe
Denise Case <denisecase@gmail.com>
2020-06-11 15:14:09 -0500

    preload & update workbox

--- service-worker.js
+++ service-worker.js
@@ -25,9 +25,7 @@
  */
 
 // eslint-disable-next-line no-undef
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
-)
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
 
 if (workbox) {
   console.log('Service worker Workbox loaded', workbox.routing)
