7c89c5f651bd658149b8e8dcd9336e75a5c9d0b5
Denise Case <denisecase@gmail.com>
2020-06-20 20:29:26 -0500

    moving data into module

---
+++ init.js
@@ -0,0 +1,11 @@
+// export init object with the initialization data
+
+export default {
+  mod1: 'Course intro, big data, shell processing',
+  mod2: 'Hadoop (HDFS + MapReduce), ecosystem, Pig, Hive,Impala, Hue',
+  mod3: 'ZooKeeper, Kafka, Java apps',
+  mod4: 'Python, map-reduce, architectures',
+  mod5: 'Apache Spark',
+  mod6: 'Demonstrations and peer review',
+  mod7: ' Optional certifications or Splunk, final reflection',
+};
--- web-components/nw-syllabus-outline.js
+++ web-components/nw-syllabus-outline.js
@@ -1,6 +1,10 @@
-class nwSyllabusOutline extends HTMLElement {
+// import default object from init.js with a local name; no extension needed
+import init from '../init.js';
+
+export default class nwSyllabusOutline extends HTMLElement {
   constructor() {
     super();
+
     this.innerHTML = `
     <h2>Course Outline</h2>
 
@@ -15,31 +19,31 @@ class nwSyllabusOutline extends HTMLElement {
     </thead>
     <tr class="d-flex">
       <td class="col-3 ">1</td>
-      <td class="col-9 "> ${this.getAttribute('mod1')} </td>
+      <td class="col-9 "> ${init.mod1} </td>
     </tr>
     <tr class="d-flex">
       <td class="col-3 ">2</td>
-      <td class="col-9 "> ${this.getAttribute('mod2')} </td>
+      <td class="col-9 "> ${init.mod2} </td>
     </tr>
     <tr class="d-flex">
       <td class="col-3 ">3</td>
-      <td class="col-9 "> ${this.getAttribute('mod3')} </td>
+      <td class="col-9 "> ${init.mod3} </td>
     </tr>
     <tr class="d-flex">
       <td class="col-3 ">4</td>
-      <td class="col-9 "> ${this.getAttribute('mod4')} </td>
+      <td class="col-9 "> ${init.mod4} </td>
     </tr>
     <tr class="d-flex">
       <td class="col-3 ">5</td>
-      <td class="col-9 "> ${this.getAttribute('mod5')} </td>
+      <td class="col-9 "> ${init.mod5} </td>
     </tr>
     <tr class="d-flex">
       <td class="col-3 ">6</td>
-      <td class="col-9 "> ${this.getAttribute('mod6')} </td>
+      <td class="col-9 "> ${init.mod6} </td>
     </tr>
     <tr class="d-flex">
       <td class="col-3 ">7</td>
-      <td class="col-9 "> ${this.getAttribute('mod7')} </td>
+      <td class="col-9 "> ${init.mod7} </td>
     </tr>
   </table>
 
