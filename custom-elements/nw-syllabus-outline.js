f29b921b2c9c5c0a7874b2834e62849d3b2c65ad
Denise Case <denisecase@gmail.com>
2020-06-23 20:23:04 -0500

    clean module content data

--- custom-elements/nw-syllabus-outline.js
+++ custom-elements/nw-syllabus-outline.js
@@ -5,6 +5,17 @@ export default class nwSyllabusOutline extends HTMLElement {
   constructor() {
     super();
 
+    // console.error('data - before : ', JSON.stringify(initOutline));
+
+    const cleanDataObject = Object.fromEntries(
+      Object.entries(initOutline).map(([k, v]) => [
+        k,
+        v.replace(/[^a-zA-Z0-9-, ]/g, ''),
+      ]),
+    );
+
+    // console.error('data - after : ', JSON.stringify(cleanDataObject));
+
     this.innerHTML = `
     <h2>Course Outline</h2>
 
@@ -19,36 +30,34 @@ export default class nwSyllabusOutline extends HTMLElement {
     </thead>
     <tr class="d-flex">
       <td class="col-3 ">1</td>
-      <td class="col-9 "> ${initOutline.mod1} </td>
+      <td class="col-9 "> ${cleanDataObject.mod1} </td>
     </tr>
     <tr class="d-flex">
       <td class="col-3 ">2</td>
-      <td class="col-9 "> ${initOutline.mod2} </td>
+      <td class="col-9 "> ${cleanDataObject.mod2} </td>
     </tr>
     <tr class="d-flex">
       <td class="col-3 ">3</td>
-      <td class="col-9 "> ${initOutline.mod3} </td>
+      <td class="col-9 "> ${cleanDataObject.mod3} </td>
     </tr>
     <tr class="d-flex">
       <td class="col-3 ">4</td>
-      <td class="col-9 "> ${initOutline.mod4} </td>
+      <td class="col-9 "> ${cleanDataObject.mod4} </td>
     </tr>
     <tr class="d-flex">
       <td class="col-3 ">5</td>
-      <td class="col-9 "> ${initOutline.mod5} </td>
+      <td class="col-9 "> ${cleanDataObject.mod5} </td>
     </tr>
     <tr class="d-flex">
       <td class="col-3 ">6</td>
-      <td class="col-9 "> ${initOutline.mod6} </td>
+      <td class="col-9 "> ${cleanDataObject.mod6} </td>
     </tr>
     <tr class="d-flex">
       <td class="col-3 ">7</td>
-      <td class="col-9 "> ${initOutline.mod7} </td>
+      <td class="col-9 "> ${cleanDataObject.mod7} </td>
     </tr>
   </table>
-
   <br>
-
     `;
   }
 }
--- init-outline.js
+++ init-outline.js
@@ -1,11 +1,11 @@
 // export init object with the initialization data
 
 export default {
-  mod1: 'Course intro, big data, shell processing',
-  mod2: 'Hadoop (HDFS + MapReduce), ecosystem, Pig, Hive,Impala, Hue',
+  mod1: 'Course <script> intro, big data, shell processing',
+  mod2: 'Hadoop (HDFS and MapReduce), ecosystem, Pig, Hive,Impala, Hue',
   mod3: 'ZooKeeper, Kafka, Java apps',
   mod4: 'Python, map-reduce, architectures',
   mod5: 'Apache Spark',
   mod6: 'Demonstrations and peer review',
-  mod7: ' Optional certifications or Splunk, final reflection',
+  mod7: 'Optional certifications or Splunk, final reflection',
 };
