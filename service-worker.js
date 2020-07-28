a36dadc7871f822cace4b7590ccae754ed6c3b55
Denise Case <denisecase@gmail.com>
2020-07-28 13:19:37 -0500

    add annotation in sw.js for global workbox (code quality)

--- service-worker.js
+++ service-worker.js
@@ -33,6 +33,7 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js',
 );
 
+/*global workbox*/
 if (workbox) {
   // console.log('Service worker Workbox loaded', workbox.routing);
 
