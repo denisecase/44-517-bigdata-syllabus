bd08eace63916d0e6519597432c8ac1ef7a453e2
Denise Case <denisecase@users.noreply.github.com>
2021-01-09 09:35:17 -0600

    Spring schedule

--- custom-elements/nw-syllabus-schedule.js
+++ custom-elements/nw-syllabus-schedule.js
@@ -18,87 +18,87 @@ class nwSyllabusSchedule extends HTMLElement {
   </thead>
   <tr class="d-flex">
     <td class="col-2 ">1</td>
-    <td class="col-4 ">Wed 8-19</td>
-    <td class="col-6 ">Module 1 begins, assignments due first day</td>
+    <td class="col-4 ">Wed 1-13</td>
+    <td class="col-6 ">Module 1 begins Wed, assignments due first day</td>
   </tr>
   <tr class="d-flex">
   <td class="col-2 ">2</td>
-  <td class="col-4 ">Mon 8-24</td>
+  <td class="col-4 ">Mon 1-18</td>
   <td class="col-6 ">Module 1 continues</td>
 </tr>
 <tr class="d-flex">
 <td class="col-2 ">3</td>
-<td class="col-4 ">Mon 8-31</td>
+<td class="col-4 ">Mon 1-25</td>
 <td class="col-6 ">Module 1 continues</td>
 </tr>
 <tr class="d-flex">
 <td class="col-2 ">4</td>
-<td class="col-4 ">Mon 9-7</td>
+<td class="col-4 ">Mon 2-1</td>
 <td class="col-6 ">No class Monday, Module 2 begins</td>
 </tr>
 <tr class="d-flex">
 <td class="col-2 ">5</td>
-<td class="col-4 ">Mon 9-14</td>
+<td class="col-4 ">Mon 2-8</td>
 <td class="col-6 ">Module 2 continues</td>
 </tr>
 <tr class="d-flex">
 <td class="col-2 ">6</td>
-<td class="col-4 ">Mon 9-21</td>
+<td class="col-4 ">Mon 2-15/td>
 <td class="col-6 ">Module 3 begins</td>
 </tr>
 <tr class="d-flex">
 <td class="col-2 ">7</td>
-<td class="col-4 ">Mon 9-28</td>
+<td class="col-4 ">Mon 2-22</td>
 <td class="col-6 ">Module 3 continues</td>
 </tr>
 <tr class="d-flex">
 <td class="col-2 ">8</td>
-<td class="col-4 ">Mon 10-5</td>
-<td class="col-6 ">Module 4 begins, no class Friday (walkout)</td>
+<td class="col-4 ">Mon 3-1</td>
+<td class="col-6 ">Module 4 begins</td>
 </tr>
 <tr class="d-flex">
 <td class="col-2 ">9</td>
-<td class="col-4 ">Mon 10-12</td>
-<td class="col-6 ">Module 4 continues</td>
+<td class="col-4 ">Mon 3-8</td>
+<td class="col-6 ">No class Monday, Module 4 continues</td>
 </tr>
 <tr class="d-flex">
 <td class="col-2 ">10</td>
-<td class="col-4 ">Mon 10-19</td>
+<td class="col-4 ">Mon 3-15</td>
 <td class="col-6 ">Module 5 begins</td>
 </tr>
 <tr class="d-flex">
 <td class="col-2 ">11</td>
-<td class="col-4 ">Mon 10-26</td>
+<td class="col-4 ">Mon 3-22</td>
 <td class="col-6 ">Module 5 continues</td>
 </tr>
 <tr class="d-flex">
 <td class="col-2 ">12</td>
-<td class="col-4 ">Mon 11-2</td>
+<td class="col-4 ">Mon 3-29</td>
 <td class="col-6 ">Module 6 begins</td>
 </tr>
 <tr class="d-flex">
 <td class="col-2 ">13</td>
-<td class="col-4 ">Mon 11-9</td>
+<td class="col-4 ">Mon 4-5</td>
 <td class="col-6 ">Module 6 continues</td>
 </tr>
 <tr class="d-flex">
 <td class="col-2 ">14</td>
-<td class="col-4 ">Mon 11-16</td>
+<td class="col-4 ">Mon 4-12</td>
 <td class="col-6 ">Module 7 begins</td>
 </tr>
 <tr class="d-flex">
 <td class="col-2 ">15</td>
-<td class="col-4 ">Mon 11-23</td>
-<td class="col-6 ">Thanksgiving break (no class Wed-Thr-Fri)</td>
+<td class="col-4 ">Mon 4-19</td>
+<td class="col-6 ">Module 7 continues</td>
 </tr>
 <tr class="d-flex">
 <td class="col-2 ">16</td>
-<td class="col-4 ">Mon 1-30</td>
-<td class="col-6 ">Online prep week (no new content, no in-person classes)</td>
+<td class="col-4 ">Mon 4-26</td>
+<td class="col-6 ">Prep week (no class)</td>
 </tr>
 <tr class="d-flex">
 <td class="col-2 ">17</td>
-<td class="col-4 ">Mon 12-7</td>
+<td class="col-4 ">Mon 5-3</td>
 <td class="col-6 ">Online finals week (no late submissions)</td>
 </tr>
 
