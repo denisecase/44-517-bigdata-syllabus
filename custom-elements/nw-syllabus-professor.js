fa533a5414134ed54f95ce129f939915bc40c7c5
Denise Case <denisecase@users.noreply.github.com>
2021-01-13 08:07:19 -0600

    office hours

--- custom-elements/nw-syllabus-professor.js
+++ custom-elements/nw-syllabus-professor.js
@@ -26,11 +26,10 @@ Office Hours
 <a href=
 "https://docs.google.com/spreadsheets/d/1dGFW0ybTrUKMN6U4vu8I9G9efUE1ZJz8ktDyOTJTOA0/edit?usp=sharing"
 > (link)</a><br>
+  MW 10-10:50 AM<br>
+  MW 11-11:50 AM<br>
   MW 3-3:50 PM<br>
-  MW 4-4:50 PM<br>
-  Tu 12:30-1:20 PM<br>
-  Tu 1:30-2:20 PM<br>
-  Tu 2:30-3:20 PM<br>
+  M  4-4:50 PM<br>
 `;
   }
 }
